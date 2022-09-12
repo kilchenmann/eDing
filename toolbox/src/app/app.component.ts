2345a67cc1cb37b0c3e1486109f0171113cdadb5
Kilchenmann <github@milchkannen.ch>
2022-09-12 08:46:42 +0200

    style(nav): changed little details in nav

--- toolbox/src/app/app.component.ts
+++ toolbox/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavItem } from './shared/models/nav-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'toolbox';
+  navItems: NavItem[] = [
+    {
+      label: "XSD to TS",
+      route: "xsd2ts"
+    },
+    {
+      label: "XML to JSON",
+      route: "xml2json"
+    }
+  ]
 }
