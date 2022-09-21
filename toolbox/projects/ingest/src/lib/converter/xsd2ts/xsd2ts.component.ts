e499b71fd0c94e376a969401b84c5e6ff1b0818d
Kilchenmann <github@milchkannen.ch>
2022-09-21 16:07:21 +0200

    chore(lib): same setup in xsd2ts as in xml2json

--- toolbox/projects/ingest/src/lib/converter/xsd2ts/xsd2ts.component.ts
+++ toolbox/projects/ingest/src/lib/converter/xsd2ts/xsd2ts.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ErrorMessage } from '../../shared';
 
 @Component({
   selector: 'avd-xsd2ts',
   templateUrl: './xsd2ts.component.html',
   styleUrls: ['./xsd2ts.component.scss']
 })
-export class Xsd2tsComponent implements OnInit {
+export class Xsd2tsComponent {
 
-  constructor() { }
+  obj = {};
 
-  ngOnInit(): void {
+  converterError = false;
+
+  error: ErrorMessage = {
+    status: 415,
+    title: 'Medientyp wird nicht unterstützt',
+    text: 'Beim Konvertieren ist ein Fehler aufgetreten. Wahrscheinlich handelt es sich nicht um valides XML.'
+  };
+
+  // define form
+  form: FormGroup = this.fb.group({
+    xsd: ['', Validators.required],
+    tsi: ['']
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  convert() {
+
+    // reset error status
+    this.converterError = false;
+
+    const parser = new DOMParser();
+    const xsd = parser.parseFromString(this.form.value['xsd'], 'text/xml');
+    // this.obj = this.ngxXml2jsonService.xmlToJson(xml);
+
+    // if (this.obj.paket) {
+    //   this.form.controls['json'].setValue(JSON.stringify(this.obj, undefined, 4));
+    // } else {
+    //     this.obj = this.obj as XmlError;
+    //     this.converterError = true;
+    //     this.form.controls['json'].setValue('');
+    //     this.error.message = this.obj.html.body.parsererror.div;
+    // }
+
   }
 
 }
