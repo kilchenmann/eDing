bf08c4f419bd323ba08b4a300dca21fa0f8e4161
Kilchenmann <github@milchkannen.ch>
2022-09-28 15:41:14 +0200

    refactor(lib/app): clean up code

--- toolbox/src/app/pages/playground/playground.component.ts
+++ toolbox/src/app/pages/playground/playground.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgxXml2jsonService } from 'ngx-xml2json';
 
 @Component({
