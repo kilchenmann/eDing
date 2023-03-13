cf5807282ba07810c36b0f6c001f6c897374cafd
Kilchenmann <github@milchkannen.ch>
2023-03-13 13:05:59 +0100

    fix(windows): resolve temp path

--- src/app/app-init.service.ts
+++ src/app/app-init.service.ts
@@ -11,7 +11,7 @@ export class AppInitService {
         const os = navigator.userAgent.toLowerCase();
         switch (true) {
             case os.indexOf('win') > -1:
-                return 'c/windows/temp';
+                return '/c/Windows/Temp';
             case os.indexOf('mac') > -1:
                 return '/private/tmp';
             case os.indexOf('linux') > -1:
