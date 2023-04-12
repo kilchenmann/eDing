457efc7139deb94d3a37af163e51786017918a16
Kilchenmann <github@milchkannen.ch>
2023-04-12 23:24:48 +0200

    chore(deps): update angular material

--- src/app/features/organize/pages/organize/organize.component.spec.ts
+++ src/app/features/organize/pages/organize/organize.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatLegacyDialogRef as MatDialogRef, MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA } from '@angular/material/legacy-dialog';
 import { ElectronService } from 'ngx-electron';
 import { AppModule } from '../../../../app.module';
 import {
